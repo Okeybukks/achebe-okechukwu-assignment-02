1ce124372f588cc4897628db67b12f54b30e3c19
Achebe Okechukwu Peter <achebepeter94@gmail.com>
2022-07-02 05:17:18 +0100

    Assignment Done

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (const iterator of arrayOfNumbers) {
+        sum += iterator;
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let even_num = []
+    for (const iterator of arrayOfNumbers) {
+        if (iterator%2 == 0) {
+            even_num.push(iterator)
+        }
+    }
+    return even_num.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
